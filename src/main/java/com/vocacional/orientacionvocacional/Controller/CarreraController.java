4e1d4452e73eeb81fb2848bc67df36f32b08a453
PieroSanchez <pierosancas@gmail.com>
2024-09-19 16:47:23 -0500

    MostrarUbicaciones

--- src/main/java/com/vocacional/orientacionvocacional/Controller/CarreraController.java
+++ src/main/java/com/vocacional/orientacionvocacional/Controller/CarreraController.java
@@ -67,4 +67,10 @@ public class CarreraController {
         List<Carrera> carreras = carreraRepository.findAll();
         return ResponseEntity.ok(carreras);
     }
+
+    @GetMapping("/mostrarubicaciones")
+    public ResponseEntity<List<Ubicacion>> obtenerUbicaciones() {
+        List<Ubicacion> ubicaciones = ubicacionRepository.findAll();
+        return ResponseEntity.ok(ubicaciones);
+    }
 }
